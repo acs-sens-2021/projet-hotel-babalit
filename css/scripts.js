2d176bb6b374633cd27245536279d044176acdff
l.aliane <l.aliane@onlineformapro.fr>
2021-07-20 15:43:06 +0200

    modif

--- css/scripts.js
+++ css/scripts.js
@@ -1,7 +1,7 @@
-// Burger
-
-const navbar = document.querySelector(".navbar");
-const burger = document.querySelector(".burger");
-burger.addEventListener("click", () => {
-    navbar.classList.toggle("show-nav");
-})
+// function toggleMenu(){
+//     const navbar = document.querySelector('.navbar');
+//     const burger = document.querySelector('.burger');
+//     burger.addEventListener('click', () => {
+//     navbar.classList.toggle('show-nav');
+//     })
+    
