7f3e7d069c9bf37b50f7748d7d08a0f9ca136390
l.aliane <l.aliane@onlineformapro.fr>
2021-07-20 15:43:18 +0200

    modif

--- css/scripts.js
+++ css/scripts.js
@@ -1,7 +1,6 @@
-// function toggleMenu(){
-//     const navbar = document.querySelector('.navbar');
-//     const burger = document.querySelector('.burger');
-//     burger.addEventListener('click', () => {
-//     navbar.classList.toggle('show-nav');
-//     })
-    
+function toggleMenu(){
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+    navbar.classList.toggle('show-nav');
+    }
